6a39fd6c15ad8c03d856b00afb4ecbb79528583a
Chinin0 <cristhianarauz06@gmail.com>
2024-06-14 14:30:20 -0400

    vista de notas

--- addons/estudiante/models/models.py
+++ addons/estudiante/models/models.py
@@ -14,6 +14,8 @@ class Estudiante(models.Model):
     direccion = fields.Char(string='Dirección')
     name_colegio = fields.Many2one('estudiante.colegio', string='Colegio')
     name_curso = fields.Many2one('estudiante.curso', string='Curso')
+    nota = fields.Many2many('estudiante.nota', string='Notas')
+    materia = fields.Many2many('estudiante.materia', string='materia')
     name_tutor = fields.Many2one('estudiante.tutor', string='Tutor')
     profesor_asignado = fields.Many2one('estudiante.profesor', string='Profesor Asignado')
 
@@ -25,7 +27,7 @@ class profesor(models.Model):
     run = fields.Char(string='RUN', required=True)
     fecha_nacimiento = fields.Date(string='Fecha de Nacimiento', required=True)
     direccion = fields.Char(string='Dirección', required=True)
-    materia = fields.Many2one('estudiante.materia')
+    materia = fields.Many2one('estudiante.materia', string='Materias')
     colegio = fields.Many2one('estudiante.colegio')
     correo = correo = fields.Char(string='Correo Electrónico', required=True)
     celular = fields.Char(string='Número de Celular', required=True)
@@ -41,6 +43,19 @@ class materia(models.Model):
         ('primaria', 'Primaria'),
         ('secundaria', 'Secundaria')
     ], string='Sección', required=True)
+    profesor = fields.Many2one('estudiante.profesor', string='Profesor Asignado')
+    curso = fields.Many2one('estudiante.curso', string='curso')
+    nota = fields.Many2one('estudiante.nota', string='Notas')
+    
+class nota(models.Model):
+    _name = 'estudiante.nota'
+    _description = 'nota'
+
+    nota = fields.Float(string='Notas', required=True)
+    estudiante = fields.Many2one('estudiante.estudiante', string='estudiante', required=True)
+    materia = fields.Many2one('estudiante.materia', string='materia', required=True)
+    profesor = fields.Many2one('estudiante.profesor', string='Profesor Asignado', required=True)
+    descripcion = fields.Text(string='descripción')
 
 class Colegio(models.Model):
     _name = 'estudiante.colegio'
@@ -54,6 +69,8 @@ class Curso(models.Model):
     _description = 'Curso'
 
     name = fields.Char(string='Nombre del Curso', required=True)
+    estudiante = fields.Many2many('estudiante.estudiante', string='estudiante')
+    materia = fields.Many2many('estudiante.materia', string='materia')
     profesor_de_curso = fields.Many2one('estudiante.profesor', string='Profesor Asignado')
     descripcion = fields.Text(string='Descripción')
     
