c4e98536db2583c08b6c804fccff3e448079632f
Chinin0 <cristhianarauz06@gmail.com>
2024-06-17 14:55:44 -0400

    modificacion en AULAS

--- addons/estudiante/models/models.py
+++ addons/estudiante/models/models.py
@@ -12,8 +12,8 @@ class Estudiante(models.Model):
     correo = fields.Char(string='Correo Electrónico')
     fecha_nacimiento = fields.Date(string='Fecha de Nacimiento')
     direccion = fields.Char(string='Dirección')
-    name_colegio = fields.Many2one('estudiante.colegio', string='Colegio')
-    name_curso = fields.Many2one('estudiante.curso', string='Curso')
+    name_infraestructura = fields.Many2one('estudiante.infraestructura', string='Colegio')
+    name_aula = fields.Many2one('estudiante.aula', string='aula')
     nota = fields.Many2many('estudiante.nota', string='Notas')
     materia = fields.Many2many('estudiante.materia', string='materia')
     name_tutor = fields.Many2one('estudiante.tutor', string='Tutor')
@@ -37,7 +37,7 @@ class profesor(models.Model):
     fecha_nacimiento = fields.Date(string='Fecha de Nacimiento', required=True)
     direccion = fields.Char(string='Dirección', required=True)
     materia = fields.Many2one('estudiante.materia', string='Materias')
-    colegio = fields.Many2one('estudiante.colegio')
+    infraestructura = fields.Many2one('estudiante.infraestructura')
     correo = correo = fields.Char(string='Correo Electrónico', required=True)
     celular = fields.Char(string='Número de Celular', required=True)
     
@@ -53,7 +53,7 @@ class materia(models.Model):
         ('secundaria', 'Secundaria')
     ], string='Sección', required=True)
     profesor = fields.Many2one('estudiante.profesor', string='Profesor Asignado')
-    curso = fields.Many2one('estudiante.curso', string='curso')
+    aula = fields.Many2one('estudiante.aula', string='aula')
     nota = fields.Many2one('estudiante.nota', string='Notas')
     
 class nota(models.Model):
@@ -66,22 +66,31 @@ class nota(models.Model):
     profesor = fields.Many2one('estudiante.profesor', string='Profesor Asignado', required=True)
     descripcion = fields.Text(string='descripción')
 
-class Colegio(models.Model):
-    _name = 'estudiante.colegio'
-    _description = 'Colegio'
+class infraestructura(models.Model):
+    _name = 'estudiante.infraestructura'
+    _description = 'infraestructura'
 
-    name = fields.Char(string='Nombre del Colegio', required=True)
+    name = fields.Char(string='Nombre del colegio', required=True)
+    nro_pisos = fields.Integer(string='Numero de pisos', required=True)
     direccion = fields.Char(string='Dirección')
 
-class Curso(models.Model):
-    _name = 'estudiante.curso'
-    _description = 'Curso'
+class aula(models.Model):
+    _name = 'estudiante.aula'
+    _description = 'Aula'
 
-    name = fields.Char(string='Nombre del Curso', required=True)
-    estudiante = fields.Many2many('estudiante.estudiante', string='estudiante')
-    materia = fields.Many2many('estudiante.materia', string='materia')
-    profesor_de_curso = fields.Many2one('estudiante.profesor', string='Profesor Asignado')
+    name = fields.Char(string='Nombre del aula', required=True)
+    estudiante = fields.One2many('estudiante.estudiante','name_aula', string='Estudiantes')
+    total_estudiantes = fields.Integer(string='Total estudiantes/aula', compute='_compute_total_estudiantes', store=True)
+    materia = fields.Many2many('estudiante.materia', string='Materia')
+    profesor_de_aula = fields.Many2one('estudiante.profesor', string='Profesor Asignado')
+    nro_sillas = fields.Integer(string='Numero de sillas', required=True)
     descripcion = fields.Text(string='Descripción')
+    
+    @api.depends('estudiante')
+    def _compute_total_estudiantes(self):
+        for aula in self:
+            aula.total_estudiantes = len(aula.estudiante)
+
     
 class Tutor(models.Model):
     _name = 'estudiante.tutor'
